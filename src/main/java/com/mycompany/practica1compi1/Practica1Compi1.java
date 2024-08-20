950c3f620adec7510098307c3a539b4e312460fd
charlyus <carloslopez201831102@cunoc.edu.gt>
2024-08-20 05:42:06 -0600

    agregando archivos principales

---
+++ src/main/java/com/mycompany/practica1compi1/Practica1Compi1.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.practica1compi1;
+
+/**
+ *
+ * @author carlos
+ */
+public class Practica1Compi1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
